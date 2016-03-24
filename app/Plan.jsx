bc8b9adf4a068dce1e77d7a1a3d5a9e1fe71d060
yongxiaolong <yongxiaolong@egret.com>
2016-03-24 16:31:36 +0800

    add css

--- app/Plan.jsx
+++ app/Plan.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './plan.css';
 
 export default class Plan extends React.Component {
 	render() {
-		return (<div>ffsdfsd</div>);
+		return (
+			<div>
+				<div className="header">
+				    <div>昨天</div>
+				    <div>2016年3月3日</div>
+				    <div>明天</div>
+				</div>
+				<div>
+				</div>
+			</div>
+		);
 	}
 }
