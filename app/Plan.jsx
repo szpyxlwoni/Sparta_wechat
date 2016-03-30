ef247cc452b1ea0ab5146f24ef0df4662a3cab26
szpyxlwoni <blasphemymoon@gmail.com>
2016-03-31 00:02:21 +0800

    complete page tag

--- app/Plan.jsx
+++ app/Plan.jsx
@@ -10,8 +10,22 @@ export default class Plan extends React.Component {
 				    <div>2016年3月3日</div>
 				    <div>明天</div>
 				</div>
-				<div>
+				<div className="plan-setting">
+				    <div><select><option>工作日</option></select></div>
+				    <div>设置</div>
+				    <div>日历</div>
+				    <div>清空</div>
 				</div>
+				<div className="plan">
+				    <div>7:00</div>
+				    <div>学习</div>
+				    <div>工作</div>
+				    <div>运动</div>
+				    <div>娱乐</div>
+				    <div>休息</div>
+				    <div>无效</div>
+				</div>
+				<div className="plus-plan"></div>
 			</div>
 		);
 	}
